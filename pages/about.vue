a78e113672d84a851891e11b042b96abdf240635
Chikara Onodera <chikara@shikake.com>
2022-10-31 19:26:10 +0900

    aboutページの更新

--- pages/about.vue
+++ pages/about.vue
@@ -8,7 +8,7 @@
     </header>
 
     <section id="feature" class="container mx-auto mt-20 max-w-5xl">
-      <h2 class="text-2xl border-b border-solid border-amber-400 pb-2 text-center">宮崎餃子の特色</h2>
+      <h2 class="text-2xl border-b border-solid border-amber-400 pb-2 text-center font-serif font-black">宮崎餃子の特色</h2>
       <p>
         宮崎県の日照時間や快晴日数・平均気温は全国トップクラスで、食料自給率や土地生産性が全国１位と、農畜産業が盛んです。
       </p>
@@ -55,69 +55,242 @@
     </section>
 
     <section id="organization" class="container mx-auto mt-20 max-w-5xl">
-      <h2 class="text-2xl border-b border-solid border-amber-400 pb-2 text-center">宮崎県ひなた餃子連合会</h2>
-      <h3 class="mt-4">宮崎県ひなた餃子連合会は、宮崎県産の多種多様で高品質な食材が包まれた宮崎餃子のブランド化を目指しています。</h3>
-      <p class="mt-4">
         <img src="/img/logo-1200s.jpg" alt="" class="w-48 mx-auto" />
-      </p>
+      <h2 class="text-2xl border-b border-solid border-amber-400 pb-2 text-center font-serif font-black">宮崎県ひなた餃子連合会</h2>
+      <p class="mt-4">宮崎県ひなた餃子連合会は、宮崎県産の多種多様で高品質な食材が包まれた宮崎餃子のブランド化を目指しています。</p>
       <div class="grid sm:grid-cols-2 gap-4">
         <section>
           <h3 class="mt-8 text-xl">組織概要</h3>
           <dl class="mt-4">
             <dt>名称</dt>
-            <dd>宮崎県ひなた餃子連合会</dd>
+            <dd>
+              <p>宮崎県ひなた餃子連合会</p>
+            </dd>
           </dl>
           <dl>
             <dt>設立</dt>
-            <dd>2022年1月</dd>
+            <dd>
+              <div>2022年<span class="text-xs">（令和4年）</span>1月</div>
+            </dd>
           </dl>
           <dl>
-            <dt>代表者</dt>
-            <dd>武末哲治</dd>
+            <dt>会長</dt>
+            <dd>
+              <p>
+                <span class="w-24">武末 哲治</span>
+                <span class="text-sm">（餃子のまち高鍋推進協議会 副会長）</span></p>
+            </dd>
+          </dl>
+          <dl>
+            <dt>副会長</dt>
+            <dd>
+              <p>
+                <span class="w-24">渡辺 愛香</span>
+                <span class="text-sm">（宮崎市ぎょうざ協議会 会長）</span>
+              </p>
+              <p>
+                <span class="w-24">馬渡 陽一郎</span>
+                <span class="text-sm">（餃子のまち高鍋推進協議会 会長）</span>
+              </p>
+              <p>
+                <span class="w-24">成合 重信</span>
+                <span class="text-sm">（県北ブロック 支部長）</span>
+              </p>
+              <p>
+                <span class="w-24">山之口 祐仁</span>
+                <span class="text-sm">（県西ブロック 支部長）</span>
+              </p>
+            </dd>
+          </dl>
+          <dl>
+            <dt>広報部長</dt>
+            <dd>
+              <p>
+                <span class="w-24"></span>
+                <span class="text-sm"></span>
+              </p>
+            </dd>
+          </dl>
+          <dl>
+            <dt>監事</dt>
+            <dd>
+              <p>
+                <span class="w-24">平原 大</span>
+                <span class="text-sm">（宮崎市ぎょうざ協議会 理事）</span>
+              </p>
+            </dd>
           </dl>
           <dl>
             <dt>事務局</dt>
-            <dd>高鍋商工会議所</dd>
+            <dd>
+              <p>高鍋商工会議所</p>
+              <p class="text-sm">〒884-0006<br />宮崎県児湯郡高鍋町大字上江8335番地2</p>
+            </dd>
+          </dl>
+          <dl>
+            <dt>Mail</dt>
+            <dd>
+              <p><nuxt-link to="/contact/">{{ mail('info') }}</nuxt-link></p>
+            </dd>
           </dl>
         </section>
 
         <section>
           <h3 class="mt-8 text-xl">支部</h3>
 
-          <dl class="mt-4">
-            <dt>県央支部</dt>
-            <dd>
-              <p>支部長： 馬渡陽一郎</p>
-              <p>広報部長：</p>
-            </dd>
-          </dl>
-          <dl>
-            <dt>宮崎市支部</dt>
-            <dd>
-              <p>支部長：渡辺愛香</p>
-              <p>広報部長：</p>
-            </dd>
-          </dl>
-          <dl>
-            <dt>県北支部</dt>
-            <dd>
-              <p>支部長：</p>
-              <p>広報部長：</p>
-            </dd>
-          </dl>
-          <dl>
-            <dt>県西・県南支部</dt>
-            <dd>
-              <p>支部長：</p>
-              <p>広報部長：</p>
-            </dd>
-          </dl>
+          <section class="mt-4"> 
+            <h4 class="text-lg">宮崎市ぎょうざ協議会</h4>
+            <dl class="border-amber-400">
+              <dt>会長</dt>
+              <dd>
+                <p>
+                  <span>渡辺 愛香</span>
+                  <span class="text-sm">（株式会社 屋台骨 専務取締役）</span>
+                </p>
+              </dd>
+            </dl>
+            <dl>
+              <dt>広報</dt>
+              <dd>
+                <p>
+                </p>
+              </dd>
+            </dl>
+            <dl>
+              <dt>事務局</dt>
+              <dd>
+                <p>公益社団法人 宮崎市観光協会</p>
+                <p class="text-sm">〒880-0811<br />宮崎県宮崎市錦町１−１−１０<br />宮崎グリーンスフィア壱番館</p>
+                <p class="text-sm">TEL: 0985-20-8658</p>
+              </dd>
+            </dl>
+            <dl>
+            <dt>Mail</dt>
+              <dd>
+                <p><nuxt-link to="/contact/">{{ mail('city') }}</nuxt-link></p>
+              </dd>
+            </dl>
+          </section>
+          <section class="mt-4">
+            <h4 class="text-lg">餃子のまち高鍋推進協議会</h4>
+            <dl class="border-amber-400">
+              <dt>会長</dt>
+              <dd>
+                <p>
+                  <span class="w-24">馬渡 陽一郎</span>
+                  <span class="text-sm">（株式会社 餃子の馬渡 代表取締役）</span>
+                </p>
+              </dd>
+            </dl>
+            <dl>
+              <dt>広報</dt>
+              <dd>
+                <p>
+                  <span class="w-24">清水 一樹</span>
+                  <span class="text-sm">（ぎょうざの樹樹 副代表）</span>
+                </p>
+              </dd>
+            </dl>
+            <dl>
+              <dt>事務局</dt>
+              <dd>
+                <p>高鍋商工会議所</p>
+                <p class="text-sm">〒884-0006<br />宮崎県児湯郡高鍋町大字上江8335番地2</p>
+                <p class="text-sm">TEL: 0983-22-1333</p>
+              </dd>
+            </dl>
+            <dl>
+              <dt>Mail</dt>
+              <dd>
+                <p><nuxt-link to="/contact/">{{ mail('center') }}</nuxt-link></p>
+              </dd>
+            </dl>
+          </section>
+          <section class="mt-4">
+            <h4 class="text-lg">県北ブロック</h4>
+            <dl class="border-amber-400">
+              <dt>支部長</dt>
+              <dd>
+                <p>
+                  <span class="w-24">成合 重信</span>
+                  <span class="text-sm">（居酒屋 日向の里 代表）</span>
+                </p>
+              </dd>
+            </dl>
+            <dl>
+              <dt>広報</dt>
+              <dd>
+                <p></p>
+              </dd>
+            </dl>
+            <dl>
+              <dt>事務局</dt>
+              <dd>
+                <p>居酒屋 日向の里</p>
+                <p class="text-sm">〒883-0044<br />宮崎県日向市上町5-12</p>
+                <p class="text-sm">TEL: 0982-53-8793</p>
+              </dd>
+            </dl>
+            <dl>
+              <dt>Mail</dt>
+              <dd>
+                <p><nuxt-link to="/contact/">{{ mail('north') }}</nuxt-link></p>
+              </dd>
+            </dl>
+          </section>
+          <section class="mt-4">
+            <h4 class="text-lg">県西ブロック</h4>
+            <dl class="border-amber-400">
+              <dt>支部長</dt>
+              <dd>
+                <p>
+                  <span class="w-24">山之口 祐仁</span>
+                  <span class="text-sm">（有限会社 山之口畜産 取締役）</span>
+                </p>
+              </dd>
+            </dl>
+            <dl>
+              <dt>広報</dt>
+              <dd>
+                <p></p>
+              </dd>
+            </dl>
+            <dl>
+              <dt>事務局</dt>
+              <dd>
+                <p>有限会社 山之口畜産</p>
+                <p class="text-sm">〒886-0003<br />宮崎県小林市堤2319-2</p>
+                <p class="text-sm">TEL: 0984-24-1183</p>
+              </dd>
+            </dl>
+            <dl>
+              <dt>Mail</dt>
+              <dd>
+                <p><nuxt-link to="/contact/">{{ mail('west') }}</nuxt-link></p>
+              </dd>
+            </dl>
+          </section>
         </section>
       </div>
+
+      <section class="mt-20">
+        <h1 class="text-center">
+          <p class="font-enTitle text-6xl">Contact</p>
+          <p class="font-serif text-xl">お問い合わせ</p>
+        </h1>
+        <div class="mt-4 text-center">
+          <NuxtLink to="/contact/" class="button">
+            <font-awesome-icon icon="fa-solid fa-paper-plane" />
+            お問い合わせフォーム
+            <font-awesome-icon icon="fa-solid fa-angle-right" class="absolute right-4 top-1/2 -translate-y-1/2" />
+          </NuxtLink>
+        </div>
+      </section>
+
     </section>
 
-    <section id="history" class="container mx-auto max-w-5xl">
-      <h3 class="mt-8 text-xl">沿革</h3>
+    <section id="history" class="mt-20 container mx-auto max-w-5xl">
+      <h3 class="text-xl">沿革</h3>
       <dl class="mt-4">
         <dt>2022年<br /><span class="text-xs">（令和4年）</span></dt>
         <dd>
@@ -149,6 +322,11 @@ export default {
       ],
     }
   },
+  methods: {
+    mail(name) {
+      return name + '@miyazakigyoza.jp'
+    },
+  }
 }
 </script>
 
@@ -167,10 +345,16 @@ export default {
   @apply w-full;
 }
 #organization dt {
-  @apply sm:w-32 pb-1 sm:pb-2 text-sm;
+  @apply sm:w-20 pb-1 sm:pb-2 text-sm align-text-top;
 }
 #organization dd {
   @apply w-auto pb-2;
+}
+#organization dd p {
+  @apply flex flex-col sm:flex-row justify-start items-start sm:items-center;
+}
+#organization span {
+  @apply inline-block;
 }
 
 #history dl {
@@ -194,5 +378,9 @@ export default {
 }
 #history dd dd {
   @apply col-span-10 sm:col-span-11;
+}
+
+.button {
+  @apply relative inline-block w-full max-w-xs py-2 bg-orange-500 text-white text-center no-underline;
 }
 </style>
