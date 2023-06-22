beb46a1a1968250d6f932711422de8bd3034c6c9
Chikara Onodera <chikara@shikake.com>
2023-06-22 19:04:03 +0900

    宮崎餃子の日テストアップ

---
+++ pages/campaign.vue
@@ -0,0 +1,163 @@
+<template>
+  <main class="pt-16">
+    <header>
+      <h1 class="text-center">
+        <p class="font-enTitle text-6xl">Campaign</p>
+        <p class="font-serif text-xl">宮崎ぎょうざの日</p>
+      </h1>
+    </header>
+
+    <section id="feature" class="container mx-auto mt-20 max-w-5xl">
+      <h2 class="text-2xl border-b border-solid border-amber-400 pb-2 text-center font-serif font-black">宮崎ぎょうざの日</h2>
+      <h3 class="mt-4 text-2xl text-center">毎年 ８月２９日・３０日・３１日</h3>
+      <img src="/about/ingredients.jpg" alt="食材イメージ" class="w-full sm:w-96 float-none sm:float-right sm:ml-4 sm:mb-4" />
+      <p>
+        全国屈指の出荷量である宮崎県産の豚肉・牛肉・鶏肉や、日照時間が長い宮崎で太陽の栄養が詰まったキャベツ・ニラ・ニンニクなどの野菜を、餃子の具材としてたっぷり使うことで、宮崎の餃子は美味しいということをアピールするため、
+      </p>
+      <p>
+        ８月の肉（ニク）の日の<span class="text-xl">【２９日】</span>
+      </p>
+      <p>
+        宮崎の「み（３）」と「餃子の形の（０）」を<br />組み合わせた<span class="text-xl">【３０日】</span>
+      </p>
+      <p>
+        野菜（ヤサイ）の日の<span class="text-xl">【８月３１日】</span>
+      </p>
+      <p>
+        の３日間を「宮崎ぎょうざの日」として制定し、 <a href="https://www.kinenbi.gr.jp/" target="_blank" class="underline">一般社団法人記念日協会</a>に登録しました。
+      </p>
+      <p class="clear-both">
+        毎年この３日間は、世帯あたりのぎょうざ支出金額・購入頻度が2021年・2022年で連続日本一<span class="text-xs">※</span>である「宮崎ぎょうざ」を、全国でより多くの方に認知いただくための特別なキャンペーンを開催していきます。<br />
+      </p>
+      <p class="text-xs">
+        ※総務省統計局が発表する家計調査（二人以上の世帯）による
+      </p>
+
+
+      <h2 class="mt-12 text-2xl border-b border-solid border-amber-400 pb-2 text-center font-serif font-black">
+        宮崎ぎょうざの日<br />2023年参加店舗
+      </h2>
+      <dl>
+        <dt>餃子の馬渡</dt>
+        <dd>
+          <p>(1)オンラインショップ　期間中 全商品1割引</p>
+          <p>(2)店舗　全商品5%OFF（高鍋本店のみ）</p>
+        </dd>
+
+        <dt>たかなべギョーザ</dt>
+        <dd>
+          <p>オンラインショップ…冷凍ギョーザ3000円以上のお買い上げで全国一律送料無料</p>
+          <p>店舗でのご購入…冷凍ギョーザ3000円以上のお買い上げで全国一律送料無料または オリジナルエコバッグプレゼント</p>
+        </dd>
+
+        <dt>屋台骨</dt>
+        <dd>
+          <p>オンライン全て送料無料</p>
+          <p>屋台骨本店、市場販売所、生餃子ショップアピール　全品10%OFF</p>
+        </dd>
+
+        <dt>鶏専家　一本気</dt>
+        <dd>
+          <p>オンラインストアで商品をお買い上げのお客様へ　一本気鶏餃子を１パックプレゼント</p>
+          <p>ご来店のお客様へもれなく焼き餃子を提供いたします。</p>
+        </dd>
+
+        <dt>中国料理樹樹（ぎょうざの樹樹）</dt>
+        <dd>
+          <p>ウェブサイト：期間中、樹樹のぎょうざ30個送料無料</p>
+          <p>店舗：期間中、餃子各種2個増量</p>
+        </dd>
+
+        <dt>有限会社 青島食肉食鳥</dt>
+        <dd>
+          <p>期間中、ご来店ならびにウェブサイトで金豚餃子をお買い上げのお客様に、新商品 エビ餃子3ｹ プレゼント。</p>
+        </dd>
+
+        <dt>餃子の里</dt>
+        <dd>
+          <p>期間中、ウェブサイトでご購入いただいたお客様に、へべす餃子6個入りをプレゼント。</p>
+        </dd>
+      </dl>
+    </section>
+
+    <section id="feature" class="container mx-auto mt-20 max-w-5xl">
+      <h2 class="text-2xl border-b border-solid border-amber-400 pb-2 text-center font-serif font-black">餃子の日</h2>
+      <h3 class="mt-4 text-2xl text-center">毎月 ３日</h3>
+      <img src="/img/gyozaday-monthly.png" alt="" class="w-full sm:w-96 mx-auto" />
+
+      <p>
+        宮崎県ひなた餃子連合会では、毎月3日を「ぎょうざの日」としてPR活動に取り組むほか、宮崎県産原料を使った「宮崎餃子」の認定基準を策定し、将来的なブランド化を目指しています。
+      </p>
+
+    </section>
+
+    <section class="mt-20">
+        <h1 class="text-center">
+          <p class="font-enTitle text-6xl">Contact</p>
+          <p class="font-serif text-xl">お問い合わせ</p>
+        </h1>
+        <div class="mt-4 text-center">
+          <NuxtLink to="/contact/" class="button">
+            <font-awesome-icon icon="fa-solid fa-paper-plane" />
+            お問い合わせフォーム
+            <font-awesome-icon icon="fa-solid fa-angle-right" class="absolute right-4 top-1/2 -translate-y-1/2" />
+          </NuxtLink>
+        </div>
+      </section>
+
+</main>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: '宮崎餃子の特色と宮崎県ひなた餃子連合会について',
+      meta: [
+        { hid: 'og:title', property: 'og:title', content: '宮崎餃子の特色と宮崎県ひなた餃子連合会について' },
+        { hid: 'description', name: 'description', content: '宮崎県ひなた餃子連合会は、宮崎県産の多種多様で高品質な食材が包まれた宮崎餃子のブランド化を目指しています。' },
+        { hid: 'og:description', property: 'og:description', content: '宮崎県ひなた餃子連合会は、宮崎県産の多種多様で高品質な食材が包まれた宮崎餃子のブランド化を目指しています。' },
+        { hid: 'og:type', property: 'og:type', content: 'website' },
+        { hid: 'og:url', property: 'og:url', content: 'https://miyazakigyoza.jp/about/' },
+        { hid: 'og:image', property: 'og:image', content: 'https://miyazakigyoza.jp/img/ogp.png' },
+        { name: 'twitter:card', content: 'summary_large_image' },
+      ],
+    }
+  },
+  methods: {
+    mail(name) {
+      return name + '@miyazakigyoza.jp'
+    },
+  },
+}
+</script>
+
+<style scoped lang="postcss">
+#feature p,
+#feature img {
+  @apply mt-4;
+}
+
+#feature dl {
+  @apply flex flex-col flex-wrap gap-y-1;
+  @apply pt-2;
+}
+#feature dt,
+#feature dd {
+  @apply w-full;
+}
+#feature dt {
+  @apply mt-4 pb-1 text-xl;
+}
+#feature dd {
+  @apply pb-1 pl-4;
+}
+#feature dd p {
+  @apply mt-1;
+}
+
+
+.button {
+  @apply relative inline-block w-full max-w-xs py-2 bg-orange-500 text-white text-center no-underline;
+}
+</style>
